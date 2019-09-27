e527c845faff2773e6f722484358dc3c69865810
Liam Rottkov <liamrottkov@gmail.com>
2019-09-27 16:22:27 -0400

    finished calculator

---
+++ calculator.py
@@ -0,0 +1,33 @@
+def calculator():
+    resp = input("Continue ? Y/N ")
+    if resp == "n" or resp == "N":
+        return False
+    return True
+while True:
+    print("Select operation.")
+    print("1.Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
+
+    choice = input("Enter choice(1/2/3/4):")
+
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    if choice == '1':
+        print(num1,"+",num2,"=", (num1 + num2))
+
+    elif choice == '2':
+        print(num1,"-",num2,"=", (num1 - num2))
+
+    elif choice == '3':
+        print(num1,"*",num2,"=", (num1 * num2))
+
+    elif choice == '4':
+        print(num1,"/",num2,"=", (num1 / num2))
+
+    else:
+        print("Invalid input")
+    if not calculator() :
+        break
